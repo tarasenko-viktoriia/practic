4601c18dc9a99e94e4f34ee33fd1278241f53a18
victoriataracenko0412 <victoriataracenko0412@gmail.com>
2024-04-17 14:50:46 +0300

    done

---
+++ script.js
@@ -0,0 +1,21 @@
+{
+    const userEyeColor = prompt("Введіть ваш колір очей");
+    console.log(userEyeColor);
+}
+{
+    let user;
+    let userName = "Вася"
+    user = userName;
+    console.log(user);
+}
+
+// let user = 'Вася' let age = 336 помилка - не розділено ; 
+
+let myage = 36; // помилки немає, але бажано написати myAge
+
+const BLOCK_SIZE = 14 + 50; // помилки немає, але краще написати blockSize
+const BLOCK_HEIGHT = 100; //без помилок
+if (true) {
+    var size = 15;
+}
+console.log(size);
